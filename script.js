72f95ecf774908a1efa87a58d25d495af2ed6e3c
Egabbard12 <gabbarderic27@gmail.com>
2024-12-18 16:02:21 -0600

    Cleaned up code and added skeleton for rest of content, Also now supports phone aspect ratio

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 
 document.addEventListener('DOMContentLoaded', function () {
-    const listItems = document.querySelectorAll('#navlst li');
+    const listItems = document.querySelectorAll('.clickable');
 
-    listItems.forEach(function (item) {
-        item.addEventListener('click', function () {
-            const spanText = this.querySelector('span').innerText;
-            alert('You clicked on ' + spanText);
-        });
-    });
+    listItems.forEach(span => {
+        span.addEventListener('click', () => {
+            const targetId = span.getAttribute('data-target');
+                const targetContent = document.getElementById(targetId);
+
+                // Toggle the visibility of the target content
+                if (targetContent.style.display === 'block') {
+                    targetContent.style.display = 'none';
+                } else {
+                    // Hide any currently visible content
+                    document.querySelectorAll('.content').forEach(content => {
+                        content.style.display = 'none';
+                    });
+                    // Show the clicked content
+                    targetContent.style.display = 'block';
+                }
+            });
+    })
+
+    
 });
 
